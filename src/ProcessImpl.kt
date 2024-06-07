39cb612379b07383885db36c6edf7605b1eb0112
imorda <btimka1@gmail.com>
2024-06-07 21:23:14 +0300

    Task done.

--- src/ProcessImpl.kt
+++ src/ProcessImpl.kt
@@ -4,20 +4,87 @@ package mutex
  * Distributed mutual exclusion implementation.
  * All functions are called from the single main thread.
  *
- * @author <First-Name> <Last-Name> // todo: replace with your name
+ * @author Timofey Belousov
  */
 class ProcessImpl(private val env: Environment) : Process {
+    enum class MsgType { REQ, OK }
+
+    private var forks = MutableList(env.nProcesses + 1) { pos ->
+        pos == 0 || pos >= env.processId
+    }
+    private var isLockPending = false
+    private var locked = false
+
+    private var requests = MutableList(env.nProcesses + 1) { false }
+
     override fun onMessage(srcId: Int, message: Message) {
-        /* todo: write implementation here */
+        lateinit var type: MsgType
+        message.parse {
+            type = readEnum()
+        }
+
+        when (type) {
+            MsgType.REQ -> {
+                if (!tryFlipFork(srcId)) {
+                    requests[srcId] = true
+                }
+            }
+
+            MsgType.OK -> {
+                assert(isLockPending)
+
+                forks[srcId] = true
+
+                if (!forks.contains(false)) {
+                    isLockPending = false
+                    env.locked()
+                    locked = true
+                }
+            }
+        }
     }
 
     override fun onLockRequest() {
-        /* todo: write implementation here */
-        env.locked() // call only when critical section can be entered
+        for ((i, fork) in forks.withIndex()) {
+            if (!fork) {
+                isLockPending = true
+                env.send(i) {
+                    writeEnum(MsgType.REQ)
+                }
+            }
+        }
+
+        if (!isLockPending) {
+            env.locked()
+            locked = true
+        }
     }
 
     override fun onUnlockRequest() {
-        /* todo: write implementation here */
+        locked = false
         env.unlocked()
+
+        for ((i, req) in requests.withIndex()) {
+            if (req) {
+                if (tryFlipFork(i)) {
+                    requests[i] = false
+                }
+            }
+        }
     }
+
+    private fun tryFlipFork(i: Int): Boolean {
+        if (forks[i]) {
+            if (locked || isLockPending) {
+                return false
+            }
+            forks[i] = false
+        }
+        env.send(i) {
+            writeEnum(MsgType.OK)
+        }
+
+        return true
+    }
+
 }
